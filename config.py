7e915ada7af5c8cc58c7e8fe2d56fc9622bc4d27
jeromefischer <jerome.fischer.02@gmail.com>
2020-10-18 12:20:05 +0200

    initial version of heatpump with webbot.

--- config.py
+++ config.py
@@ -1,10 +1,91 @@
 # Innoxel Weather Station Configuration
 ip = 'http://192.168.2.172'
 port = '5001'
-username = 'username'
-password = 'password'
+username = 'SOAP'
+password = 'soap'
+
+dict_room_temperature = {'UG - Waschküche': 16,
+                         'UG - Vorplatz': 20,
+                         'EG - WC': 34,
+                         'EG - Küche': 36,
+                         'EG - Wohnen': 48,
+                         'OG - Vorplatz': 56,
+                         'OG - Büro Jerome': 60,
+                         'OG - Zimmer Nord': 62,
+                         'OG - Elternzimmer': 64,
+                         'OG - Büro Jasmin': 70,
+                         'OG - Bad': 72,
+                         'OG - Elternbad': 74
+                         }
+
+dict_heater_valves = {'UG - Waschküche': [4, 0],
+                      'UG - Vorplatz': [4, 1],
+                      'EG - WC': [4, 2],
+                      'EG - Küche': [4, 3],
+                      'EG - Wohnen': [4, 4],
+                      'OG - Vorplatz': [5, 0],
+                      'OG - Büro Jerome': [5, 1],
+                      'OG - Zimmer Nord': [5, 2],
+                      'OG - Elternzimmer': [5, 3],
+                      'OG - Büro Jasmin': [5, 4],
+                      'OG - Bad': [5, 5],
+                      'OG - Elternbad': [5, 6]
+                      }
 
 # InfluxDB Configuration
 db_ip = '192.168.2.152'
 db_port = '8086'
 db_name = 'innoxel'
+
+# Heatpump
+heatpump_db_name = 'heatpump'
+heatpump_url = "http://192.168.2.223/Webserver/index.html"
+# TEMPERATUREN: parsed_html.contents[1].contents[2].contents[5].contents[0].contents[00]
+# 01: Vorlauf
+# 02: Rücklauf
+# 03: Rücklauf Soll
+# 05: Aussentemperatur
+# 06: Mitteltemperatur
+# 07: Warmwasser-Ist
+# 08: Warmwasser-Soll
+# 09: Wärmequelle-Ein
+temps = [1, 2, 3, 5, 6, 7, 8, 9]
+
+# EINGÄNGE: parsed_html.contents[1].contents[2].contents[5].contents[0].contents[01]
+
+# AUSGÄNGE: parsed_html.contents[1].contents[2].contents[5].contents[0].contents[02]
+
+# ABLAUFZEITEN: parsed_html.contents[1].contents[2].contents[5].contents[0].contents[03]
+
+# BETRIEBSSTUNDEN: parsed_html.contents[1].contents[2].contents[5].contents[0].contents[04]
+# 1: Betriebsstunden VD1
+# 2: Impulse Verdichter 1
+# 3: Laufzeit Durchschnitt VD1
+# 5: Betriebsstunden WP
+# 6: Betriebsstunden Heizung
+# 7: Betriebsstunden WW
+eingaenge = [1, 2, 5, 6, 7]
+
+# FEHLERSPEICHER: parsed_html.contents[1].contents[2].contents[5].contents[0].contents[05]
+# 1:
+
+# ABSCHALTUNGEN: parsed_html.contents[1].contents[2].contents[5].contents[0].contents[06]
+# 1:
+
+# ANLAGESTATUS: parsed_html.contents[1].contents[2].contents[5].contents[0].contents[07]
+# 5: Betriebszustand
+# 6: Leistung
+anlagestatus = [5, 6]
+
+# WÄRMEERZEUGER: parsed_html.contents[1].contents[2].contents[5].contents[0].contents[08]
+# 1: Heizung
+# 2: Warmwasser
+# 3: Gesamt
+waermeerzeugung = [1, 2, 3]
+
+heatpump_data_points = [(0, 1), (0, 2), (0, 3), (0, 5), (0, 6), (0, 7), (0, 8), (0, 9),
+                        (4, 1), (4, 2), (4, 5,), (4, 6), (4, 7),
+                        (7, 5), (7, 6),
+                        (8, 1), (8, 2), (8, 3)]
+
+
---
+++ heatpump.py
@@ -0,0 +1,112 @@
+#! /usr/bin/python3
+import datetime
+import time
+import config
+from idna import unicode
+from influxdb import InfluxDBClient
+from webbot import Browser
+from bs4 import BeautifulSoup
+
+
+def generate_json(data):
+
+    json_body = [
+        {
+            "measurement": "heatpump",
+            "location": "Neuendorf",
+            "tags": {
+                "host": "heatpump",
+                "Region": "Neuendorf"
+            },
+            "time": datetime.datetime.utcnow().isoformat(),
+            "fields": data
+        }
+    ]
+    print(json_body)
+    return json_body
+
+
+class Heatpump():
+
+    def __init__(self, url, heatpump_data_points, db_ip, db_port, db_name):
+
+        self.url = url
+        self.heatpump_data_points = heatpump_data_points
+        self.db_ip = db_ip
+        self.db_port = db_port
+        self.db_name = db_name
+
+    def get_measurements_from_heatpump(self):
+
+        web = Browser(showWindow=False) # this will navigate python to browser. if showWindow = False, it is executed headless
+        link = web.go_to(self.url)
+        id = web.type('99999', into='password_prompt_input', id='password_prompt_input')
+        next = web.click('OK', tag='span')
+        time.sleep(0.05)
+        nav = web.click('Informationen', tag='span')
+        time.sleep(0.05)
+        r = web.get_page_source()
+        web.close_current_tab()
+        return r
+
+    def parse_measurement(self, measurement_data):
+
+        parsed_html = BeautifulSoup(measurement_data, features="lxml")
+        data = {}
+        for k, i in self.heatpump_data_points:
+            measure = parsed_html.contents[1].contents[2].contents[5].contents[0].contents[k].contents[0].contents[0].contents[i].contents[0].contents[0]
+            try:
+                status = unicode(parsed_html.contents[1].contents[2].contents[5].contents[0].contents[k].contents[0].contents[0].contents[i].contents[1].contents[0])
+            except:
+                status = 'n.a.'
+
+            # remove unit from data point
+            if status.endswith('kWh'):
+                status = status[:-3]
+            elif status.endswith('°C') or status.endswith('kW'):
+                status = status[:-2]
+            elif status.endswith('h'):
+                status = status[:-1]
+            print("{}: {}".format(measure, status))
+            data[measure] = status
+
+        # check if current status is "Heizen".
+        # If so, create new data point "Leistung Heizen" and write value.
+        # If it is "Warmwasser", create new data point "Leistung Warmwasser" and write value
+        if data['Betriebszustand'] == 'Heizen':
+            data['Leistung Heizen'] = data['Leistung Ist']
+        elif data['Betriebszustand'] == 'WW':
+            data['Leistung WW'] = data['Leistung Ist']
+        # remove unused data points from dict
+        del data['Leistung Ist']
+        del data['Betriebszustand']
+
+         # since we now have only integers in the data dict as values, we can convert all values to float (required to have
+        # for influxDB
+        for m, n in data.items():
+            data[m] = float(data[m])
+
+        return data
+
+    def write_to_influx(self, json_body):
+        # InfluxDB Configuration
+        client = InfluxDBClient(host=self.db_ip, port=self.db_port, database=self.db_name)
+        # print(client.get_list_database())
+        #client.drop_database(dbname=self.db_name)
+        #client.create_database(dbname=self.db_name)
+        success = client.write_points(points=json_body, time_precision='s', database=self.db_name, protocol=u'json')
+
+        print('successfully written to db: {}'.format(success))
+        return success
+
+
+hp = Heatpump(url=config.heatpump_url,
+              heatpump_data_points=config.heatpump_data_points,
+              db_ip=config.db_ip,
+              db_port=config.db_port,
+              db_name=config.heatpump_db_name)
+
+meas = hp.get_measurements_from_heatpump()
+parsed = hp.parse_measurement(measurement_data=meas)
+json_data = generate_json(data=parsed)
+success = hp.write_to_influx(json_body=json_data)
